f64540055c3629d6dea9a3e0013e5d6b718ac2b5
Ferdeen <joe.poptiya@gmail.com>
2022-03-04 23:26:11 -0800

    Setup initial login button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import GoogleLogin from "react-google-login";
+import "./App.css";
 
 function App() {
+  const handleFailure = (response) => {
+    alert(response);
+  };
+
+  const handleLogin = (response) => {
+    console.log(response);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Google Login App</h1>
+        <div>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Log in with Google"
+            onSuccess={handleLogin}
+            onFailure={handleFailure}
+            cookiePolicy={"single_host_origin"}
+          ></GoogleLogin>
+        </div>
       </header>
     </div>
   );
