3a49e8363e9d8fe1a820cd7b3b6616645ceb2a4a
Ferdeen <joe.poptiya@gmail.com>
2022-03-05 17:47:56 -0800

    Post proxy setting that didn't work

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { OAuth2Client } = require("google-auth-library");
+const { useImperativeHandle } = require("react");
+
+dotenv.config();
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
+const app = express();
+app.use(express.json());
+
+// TODO: Change to MongoDB
+const users = [];
+
+function upsert(array, item) {
+  const i = array.findIncex((_item) => _item.email === item.email);
+  if (i > -1) array[i] = item;
+  else array.push(item);
+}
+
+app.post("/api/google-login", async (req, res) => {
+  console.log("in google login {process.env.CLIENT_ID}");
+  const { token } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+
+  const { name, email, picture } = ticket.getPayload();
+  upsert(users, { name, email, picture });
+  res.status(201).json({ name, email, picture });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `Server started on port http://localhost:${process.env.PORT || 5000}`
+  );
+});
